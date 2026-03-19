66a4a2a1e800569261c94a49ee41ca9914a3859f
Mark Kent <hellopastormark@gmail.com>
2026-03-19 09:28:06 -0400

    Fix update banner: offline detection removed, button listeners, version mismatch on update

--- functions/api/data-version.js
+++ functions/api/data-version.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-  return new Response(JSON.stringify({ version: "2026-03" }), {
+  return new Response(JSON.stringify({ version: "2026-04" }), {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*"
